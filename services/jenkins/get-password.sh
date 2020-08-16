17ace7eecb3434fa2a13ed9380a2887b2a657f19
mtopfer <matija.topfer@gmail.com>
2020-08-16 16:05:52 +0100

    Fixing container reference for get-password.sh

--- services/jenkins/get-password.sh
+++ services/jenkins/get-password.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Initial Jenkins access password:"
-docker exec -it jenkins-master cat /var/jenkins_home/secrets/initialAdminPassword
+docker exec -it jenkins-manager cat /var/jenkins_home/secrets/initialAdminPassword
